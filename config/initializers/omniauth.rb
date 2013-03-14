7d8ba01e2fe18e50f39551a747282b14f91f9d0e
Amit Thawait <amit17thawait@gmail.com>
2013-03-14 11:53:46 -0700

    Facebook app_id & secret key added

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,6 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :developer unless Rails.env.production?
+  provider :facebook, '124580891058671', '9e9c6d66e3845f163fc8972a003c5537'
+  #provider :twitter, 'CONSUMER_KEY', 'CONSUMER_SECRET'
+end
+
