12f405f8976e816ce49d9e70dd15f28f11eebe81
Amit Thawait <amit17thawait@gmail.com>
2013-04-25 07:59:02 -0700

    Using open-uri to fetch the user image instead of curl

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,10 +1,14 @@
+require 'open-uri'
+
 module ArticlesHelper
   def user_photo(provider, user_id)
   	case provider
   	when 'facebook'
-  	  JSON.parse(`curl https://graph.facebook.com/#{user_id}?fields=picture`)['picture']['data']['url']
+  	  #JSON.parse(`curl https://graph.facebook.com/#{user_id}?fields=picture`)['picture']['data']['url']
+  	  JSON.parse(open("https://graph.facebook.com/#{user_id}?fields=picture").string)['picture']['data']['url']
   	when 'github'
-  	  JSON.parse(`curl https://api.github.com/users/#{user_id}`)['avatar_url']
+  	  #JSON.parse(`curl https://api.github.com/users/#{user_id}`)['avatar_url']
+  	  JSON.parse(open("https://api.github.com/users/#{user_id}").string)['avatar_url']
   	end
   end
 end
